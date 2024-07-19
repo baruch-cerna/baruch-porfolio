0c8dd504c210466a8f23369c7cf5b217df875820
baruchalan3 <baruchalan3@gmail.com>
2024-07-19 22:34:58 +0000

    red-had-mono.css and iu-animations changed

--- public/scripts/ui-animations.ts
+++ public/scripts/ui-animations.ts
@@ -4,13 +4,13 @@ import { TextPlugin } from "gsap/TextPlugin";
 gsap.registerPlugin(TextPlugin);
 
 export const startAnimations = () => {
-  const tlYour = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
-  tlYour.to("#your", { duration: 1, text: "your", delay: 1 });
   const tlEnter = gsap.timeline();
   tlEnter.from(".enter", {
-    duration: 0.8,
+    duration: 0.6,
     y: "100%",
     opacity: 0,
     ease: "back.inOut",
   });
+  const tlYour = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
+  tlYour.to("#your", { duration: 0.7, text: "your", delay: 1.6 });
 };
