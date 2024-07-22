a74143590868080351b2cb56ad5f644b4af29c50
baruchalan3 <baruchalan3@gmail.com>
2024-07-22 23:54:21 +0000

    Mejoras en p5

--- public/scripts/p5-script.ts
+++ public/scripts/p5-script.ts
@@ -1,8 +1,11 @@
 import p5 from "p5";
 
 const sketch = (p: p5) => {
+  p.disableFriendlyErrors = true;
+
   const points: p5.Vector[] = [];
   const noiseMultiplier = 0.01;
+  let cursor: p5.Element | null;
 
   const setInitialPoints = (body: HTMLBodyElement) => {
     const totalPoints = window.devicePixelRatio * body.offsetHeight;
@@ -17,32 +20,36 @@ const sketch = (p: p5) => {
     setInitialPoints(body);
     const from = p.color(92, 99, 115);
     const to = p.color(217, 217, 217);
-    const pColor = p.lerpColor(from, to, 0.9);
+    const pColor = p.lerpColor(from, to, 0.8);
     p.stroke(pColor);
+    cursor = p.select("#v-pointer");
   };
 
   const outOfCanvas = (item: p5.Vector) =>
     item.x < 0 || item.y < 0 || item.y > p.height || item.x > p.width;
 
   p.draw = () => {
-    p.background(217, 217, 217, 2);
-    for (const po of points) {
+    p.background(217, 217, 217, 7);
+    points.forEach((po) => {
       p.point(po.x, po.y);
       const n = p.noise(po.x * noiseMultiplier, po.y * noiseMultiplier);
       const angle = p.TWO_PI * n;
-      po.x += p.cos(angle);
-      po.y += p.sin(angle);
-      po.z += 0.02;
+      po.x += Math.cos(angle);
+      po.y += Math.sin(angle);
       if (outOfCanvas(po)) {
-        po.x = p.random(p.width);
-        po.y = p.random(p.height);
+        po.x = p.width * Math.random();
+        po.y = p.height * Math.random();
       }
-    }
+    });
   };
 
   p.windowResized = () => {
     const body = document.getElementsByTagName("body")[0];
     p.resizeCanvas(body.offsetWidth, body.offsetHeight);
+  };
+
+  p.mouseMoved = () => {
+    if (cursor) cursor.position(p.mouseX, p.mouseY);
   };
 };
 
--- public/scripts/ui-animations.ts
+++ public/scripts/ui-animations.ts
@@ -13,4 +13,17 @@ export const startAnimations = () => {
   });
   const tlYour = gsap.timeline({ repeat: -1, repeatDelay: 1, yoyo: true });
   tlYour.to("#your", { duration: 0.7, text: "your", delay: 1.6 });
+  const tools: gsap.DOMTarget[] = gsap.utils.toArray(".tool");
+  tools.forEach((tool, i) => {
+    gsap.to(tool, {
+      scrollTrigger: {
+        trigger: tool,
+        start: "top center",
+        end: "+=500",
+      },
+      delay: (i + 1) * 0.1,
+      scale: 1,
+      ease: "back.inOut",
+    });
+  });
 };
