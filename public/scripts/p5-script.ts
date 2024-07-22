56cb093ec5f1487c3f4f19c6ddba16db786cc1d6
baruchalan3 <baruchalan3@gmail.com>
2024-07-22 20:48:21 +0000

    Another

--- public/scripts/p5-script.ts
+++ public/scripts/p5-script.ts
@@ -5,7 +5,7 @@ const sketch = (p: p5) => {
   const noiseMultiplier = 0.01;
 
   const setInitialPoints = (body: HTMLBodyElement) => {
-    const totalPoints = Math.round(window.devicePixelRatio * body.offsetWidth);
+    const totalPoints = window.devicePixelRatio * body.offsetHeight;
     for (let i = 0; i < totalPoints; i++) {
       points.push(p.createVector(p.random(p.width), p.random(p.height)));
     }
