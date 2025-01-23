8db7ad99264354f6993e5034dd471dca352ae548
baruchalan3 <baruchalan3@gmail.com>
2025-01-23 18:54:00 +0000

    Sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,15 @@ import cloudflare from "@astrojs/cloudflare";
 
 import compressor from "astro-compressor";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  site: 'https://baruch-cerna.dev',
   adapter: cloudflare(),
   image: {
     remotePatterns: [{ protocol: "https" }],
   },
-  integrations: [compressor()],
+  integrations: [compressor(), sitemap()],
 });
